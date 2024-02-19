3d8b8cab099e93c4905be2f507f5d96f24bf6563
Tommy.Wu <story0629@gmail.com>
2024-02-19 18:24:30 +0800

    Navbar complete

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,14 +1,32 @@
+import { useState } from "react";
+import { FiAlignJustify, FiX } from "react-icons/fi";
 import { Link } from "react-router-dom";
+import Styles from './Navbar.module.css'
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(true);
+
+  const toggleHandler = () => {
+    setIsOpen(!isOpen);
+  }
+
   return (
-    <nav>
-        <ul>
-            <li>Home</li>
-            <li>React Hook</li>
-            <li>React Classic</li>
-        </ul>
-    </nav>
+    <>
+      <nav className={`${Styles.navbar} ${isOpen ? '' : Styles.navbarClose}`}>
+        <div className={Styles.icon}>
+        {
+          isOpen ?
+          <FiX size={"2.5rem"} color="white" onClick={toggleHandler}/> :
+          <FiAlignJustify size={"2.5rem"} color="white" onClick={toggleHandler}/>
+        }
+        </div>
+          <ul>
+              <li>Home</li>
+              <li>React Hook</li>
+              <li>React Classic</li>
+          </ul>
+      </nav>
+    </>
   )
 }
 
--- src/Layout/Layout.tsx
+++ src/Layout/Layout.tsx
@@ -4,7 +4,7 @@ import { Outlet } from 'react-router-dom'
 const Layout = () => {
   return (
     <div>
-        {/* <Navbar/> */}
+        <Navbar/>
         <Outlet/>
     </div>
   )
